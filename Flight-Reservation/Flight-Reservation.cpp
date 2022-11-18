f0bbf442052efb9b05a75494c16b7e706794886e
Carl Justine F. Malabanan <carljustinem984@gmail.com>
2022-11-18 19:54:05 +0800

    jhg

--- Flight-Reservation/Flight-Reservation.cpp
+++ Flight-Reservation/Flight-Reservation.cpp
@@ -10,12 +10,14 @@ using namespace std;
 
 string uName, pWord, flightTm[5];
 
-void mainmenu(), logIn(), signUp(), menu();
+void mainmenu(), logIn(), signUp(), menu(),logOut();
 
 int main()
 {
-    //signUp();
-    //system("cls");
+    menu();
+    system("cls");
+    system("cls");
+    
     //mainmenu();
 }
 
@@ -45,14 +47,13 @@ void mainmenu() {
 
 
 void menu() {
-    //menuRepeat:
+    menuRepeat:
     int n;
     cout << "[1] Login" << endl << "[2] Sign Up" << endl << "[3] Log Out" << endl << "[4] Exit" << endl << "Choice: ";
     cin >> n;
     cin.ignore();
     switch (n)
     {
-    menuRepeat:
     case 1:
         logIn();
         break;
@@ -60,7 +61,7 @@ void menu() {
         signUp();
         break;
     case 3:
-        //logOut();
+        logOut();
         break;
     case 4:
         // exitProgram();
@@ -116,6 +117,13 @@ confirm:
     }
 }
 
+void logOut()
+{
+    return;
+    
+}
+
+
 void FlightChc() {
     string flightNm[5] = { "Jakarta, Indonesia", "Amsterdam, Netherlands", "New York, USA", "Manila, Philippines", "Osaka, Japan" };
     string flightTm[10] = { "10:00PM - 11:00PM", "12:15AM - 2:00AM", "3:00AM - 3:45AM", "4:00AM - 5:30AM", "12:00PM - 2:15PM", "8:00AM - 9:20AM", "3:00PM - 4:45PM", "5:30PM - 7:00PM", "7:15PM - 8:50PM", "9:15PM - 9:50PM" };
@@ -162,3 +170,4 @@ arriv:
         c++;
     }
 }
+
